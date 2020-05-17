042527f1c8a5a5c5252eaeabce2d8d9a28881980
René Thiel <thiel@link-innovation.de>
2020-05-17 17:30:42 +0200

    boot

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,98 @@
   <div>
     <logo />
     <h1 class="title">Yo. ql. Nuxt.js & Netlify CMS Starter</h1>
+
+    <header id="header-3" class="soft-scroll header-3" data-pg-collapsed>
+      <nav>
+        <div class="container">
+          <div
+            class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2"
+          >
+            <nav class="pull">
+              <ul class="top-nav">
+                <li>
+                  <a href="#"
+                    >Home
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"
+                    >Features
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"
+                    >Pages
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"
+                    >Portfolio
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"
+                    >Team
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+                <li>
+                  <a href="#"
+                    >Contact
+                    <span class="indicator"
+                      ><i class="fa fa-angle-right"></i></span
+                  ></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /.container-fluid -->
+      </nav>
+      <!-- /.nav -->
+      <section class="hero">
+        <div class="container">
+          <div class="brand">
+            <a href="#">
+              <img
+                src="images/brand/pgblocks-logo-nostrap.png"
+                class="brand-img img-responsive"
+              />
+            </a>
+          </div>
+          <div class="navicon" data-pg-collapsed>
+            <a id="nav-toggle" class="nav-slide-btn" href="#"><span></span></a>
+          </div>
+          <div class="col-md-8 col-md-offset-2 text-center" data-pg-collapsed>
+            <h1>rocket</h1>
+            <p class="lead">
+              Use a page-builder to speed up your page-creation process.
+            </p>
+            <a
+              href="#"
+              class="btn btn-info btn-block"
+              style="display: inline !important;"
+              >Find out more</a
+            >
+            <a
+              href="#"
+              class="btn btn-success btn-block"
+              style="display: inline !important;"
+              >Login</a
+            >
+          </div>
+        </div>
+      </section>
+    </header>
   </div>
 </template>
 
